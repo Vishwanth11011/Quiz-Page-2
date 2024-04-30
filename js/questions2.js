2a5565afd283ce0d5407e97e71f52f3b34739e2a
Vishwanth11011 <153525240+Vishwanth11011@users.noreply.github.com>
2024-04-30 23:47:24 +0530

    Add files via upload

---
+++ js/questions2.js
@@ -0,0 +1,58 @@
+let questions2  = [
+    {
+    numb: 1,
+    question2: "How to maked curved edge box?",
+    answer2: "Box-radius",
+    options2: [
+      "eXtensible Markup Language",
+      "Box-radius",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 2,
+    question2: "How to maked curved edge box?",
+    answer2: "Box-radius",
+    options2: [
+      "eXtensible Markup Language",
+      "Box-radius",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 3,
+    question2: "How to maked curved edge box?",
+    answer2: "Box-radius",
+    options2: [
+      "eXtensible Markup Language",
+      "Box-radius",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 4,
+    question2: "How to maked curved edge box?",
+    answer2: "Box-radius",
+    options2: [
+      "eXtensible Markup Language",
+      "Box-radius",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 5,
+    question2: "How to maked curved edge box?",
+    answer2: "Box-radius",
+    options2: [
+      "eXtensible Markup Language",
+      "Box-radius",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+];
---
+++ js/questions3.js
@@ -0,0 +1,57 @@
+let questions3 = [
+    {
+        numb: 1,
+        question: "How to maked curved edge box?",
+        answer: "Box-radius",
+        options: [
+          "eXtensible Markup Language",
+          "Box-radius",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      {
+        numb: 2,
+        question: "How to chnge background color of a box?",
+        answer: "backgroun-color:",
+        options: [
+          "eXtensible Markup Language",
+          "backgroun-color:",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      {
+        numb: 3,
+        question: "How to maked curved edge box?",
+        answer: "Box-radius",
+        options: [
+          "eXtensible Markup Language",
+          "Box-radius",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      {
+        numb: 4,
+        question: "How to maked curved edge box?",
+        answer: "Box-radius",
+        options: [
+          "eXtensible Markup Language",
+          "Box-radius",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      {
+        numb: 5,
+        question: "How to maked curved edge box?",
+        answer: "Box-radius",
+        options: [
+          "eXtensible Markup Language",
+          "Box-radius",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+];
